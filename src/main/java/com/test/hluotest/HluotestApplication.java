e6b30aa0aca2689bde979d9ecf515ef41b11d199
lhn <1033921491@qq.com>
2019-10-22 20:30:53 +1100

    first commit

---
+++ src/main/java/com/test/hluotest/HluotestApplication.java
@@ -0,0 +1,13 @@
+package com.test.hluotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HluotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HluotestApplication.class, args);
+	}
+
+}
