2e6483ecc50c3aaf0dff301d8b03c31766c56e0e
lhn <1033921491@qq.com>
2019-10-22 22:55:11 +1100

    test jenkins33

--- src/main/java/com/test/hluotest/HluotestApplication.java
+++ src/main/java/com/test/hluotest/HluotestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HluotestApplication {
 
 	public static void main(String[] args) {
-		System.out.println("start");
+		System.out.println("start123");
 		SpringApplication.run(HluotestApplication.class, args);
 	}
 
