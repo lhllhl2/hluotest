e0138b40ebf724a65014a71ac94fd04150f9cdd0
lhn <1033921491@qq.com>
2019-10-22 23:14:12 +1100

    test jj

--- src/main/java/com/test/hluotest/HluotestApplication.java
+++ src/main/java/com/test/hluotest/HluotestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HluotestApplication {
 
 	public static void main(String[] args) {
-		System.out.println("start123");
+		System.out.println("start123456");
 		SpringApplication.run(HluotestApplication.class, args);
 	}
 
